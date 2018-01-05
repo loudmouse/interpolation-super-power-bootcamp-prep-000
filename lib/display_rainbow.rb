8380ae60271e010f16ead975921c713d99b7275e
Nolan Neeley <nolanneeley@gmail.com>
2018-01-05 03:53:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,9 @@
 
 def display_rainbow(colors)
-  puts "R: #{colors(0)}, O: #{colors(1)}"
+  puts "R: #{colors[0]}, O: #{colors[1]}"
 end
 
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 display_rainbow(colors)
+
